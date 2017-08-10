a3572422d0f5b1b47ba6c600540214d14542789d
qqupp <paolo.picci@gmail.com>
2017-08-10 19:54:14 +0100

    blanc lines an spaces fix

--- app/controllers/EightBallController.scala
+++ app/controllers/EightBallController.scala
@@ -6,29 +6,24 @@ import play.api.mvc._
 
 import models.EightBallReply
 
-
 @Singleton
 class EightBallController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-
-
-
 
   def randomReply = Action {
     val rndString = EightBallReply.getRndBall.content
     Ok(views.txt.text(rndString))
   }
 
-
   def getMessages = Action {
-    val allMsgs = (EightBallReply.getAll map ( x => x.content ) ).fold("")((x:String, y:String) => x + "\n" + y)
+    val allMsgs = (EightBallReply.getAll map ( x => x.content ) ).
+      fold("")((x:String, y:String) => x + "\n" + y)
     Ok(views.txt.text(allMsgs))
   }
-
 
   def getMessage(id: Int) = Action {
     EightBallReply.get(id) map { x =>
       Ok(views.txt.text(x.content))
-    } getOrElse  {
+    } getOrElse {
       Ok(views.txt.text("Nothing to see here!"))
     }
   }
